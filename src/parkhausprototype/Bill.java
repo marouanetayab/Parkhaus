1692ac7cff8e394e4a4227b141c6a9f99042ae72
johannes Heimbach <heimbachjohannes@gmail.com>
2018-01-13 16:46:53 +0100

    added bill Interface and class

---
+++ src/parkhausprototype/Bill.java
@@ -0,0 +1,37 @@
+package parkhausprototype;
+
+public class Bill implements BillIF{
+	private String fahrzeugName;
+	private double kosten;
+	private double zeit;
+
+	public Bill(String fahrzeugName, double kosten, double zeit) {
+		this.fahrzeugName = fahrzeugName;
+		this.kosten = kosten;
+		this.zeit = zeit;
+	}
+
+	public String getFahrzeugName() {
+		return fahrzeugName;
+	}
+
+	public void setFahrzeugName(String name) {
+		this.fahrzeugName = name;
+	}
+
+	public double getKosten() {
+		return kosten;
+	}
+
+	public void setKosten(double kosten) {
+		this.kosten = kosten;
+	}
+
+	public double getZeit() {
+		return zeit;
+	}
+
+	public void setZeit(double zeit) {
+		this.zeit = zeit;
+	}
+}
---
+++ src/parkhausprototype/BillIF.java
@@ -0,0 +1,10 @@
+package parkhausprototype;
+
+public interface BillIF {
+	String getFahrzeugName();
+	void setFahrzeugName(String name);
+	double getKosten();
+	void setKosten(double kosten);
+	double getZeit();
+	void setZeit(double zeit);
+}
