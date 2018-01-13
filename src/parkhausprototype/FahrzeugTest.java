8a01bacffe9dbe844bff65afe8b6d81755644509
johannes Heimbach <heimbachjohannes@gmail.com>
2018-01-13 16:47:13 +0100

    added fahrzeugtest

---
+++ src/parkhausprototype/FahrzeugTest.java
@@ -0,0 +1,43 @@
+package parkhausprototype;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class FahrzeugTest {
+    Fahrzeug fahrzeug;
+
+    @Before
+    public void setUp() {
+        fahrzeug = new Fahrzeug("test1");
+    }
+
+    @Test
+    public void reset() {
+        fahrzeug.park(1);
+        fahrzeug.reset();
+        assertEquals(-1, fahrzeug.parkNR);
+        assertFalse(fahrzeug.parked);
+    }
+
+    @Test
+    public void park() {
+        fahrzeug.park(1);
+        assertEquals(1, fahrzeug.parkNR);
+        assertTrue(fahrzeug.parked);
+        assertEquals(LocalTime.now().withNano(0), fahrzeug.begin);
+    }
+
+    @Test
+    public void unpark() throws InterruptedException {
+        fahrzeug.park(1);
+        TimeUnit.SECONDS.sleep(1);
+        fahrzeug.unpark();
+        assertEquals(LocalTime.now().withNano(0), fahrzeug.end);
+        assertEquals(1, fahrzeug.duration, 0);
+    }
+}
